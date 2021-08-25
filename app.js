98511ef6f695a3792531578cbe2516f1dae7c876
sarutobexx <61440864+sarutobexx@users.noreply.github.com>
2021-08-24 21:45:59 -0300

    atualização app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set ('view engine', 'handlebars')
 
 //mongoose
-mongoose.connect('mongodb+srv://Saude:pre1958@saude.5ss1r.mongodb.net/test?authSource=admin&replicaSet=atlas-nx2mu2-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true').then(()=>{
+mongoose.connect().then(()=>{
       console.log('conectado ao mongodb')}).catch((err)=>{
                     console.log('erro ao se conectar'+err)
 })
